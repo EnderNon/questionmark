c34b0a9741788a4a3c7fc895171f0bc33a479358
endernon <ender_non@outlook.com>
2025-05-29 17:48:33 +0100

    yeah that works now even with no input

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@
 #![allow(dead_code)]
 use leptos::prelude::*;
 use leptos_router::{
+    components::{
+        Router, 
+        Route, 
+        Routes
+    },
     params::Params,
     hooks::{
         use_query
@@ -10,28 +15,38 @@ use leptos_router::{
 
 #[derive(Params,PartialEq, Clone)]
 pub struct FourUsers {
-    A: Option<String>,
-    B: Option<String>,
-    C: Option<String>,
-    D: Option<String>
+    a: Option<String>,
+    b: Option<String>,
+    c: Option<String>,
+    d: Option<String>
 }
 
 fn main() {
     console_error_panic_hook::set_once();
-    mount_to_body(|| view! { <App/> });
+    mount_to_body(|| view! { 
+        <Router>
+            <App/> 
+        </Router>
+    });
 }
 
 #[component]
 pub fn App() -> impl IntoView {
     let players_from_url = use_query::<FourUsers>();
     let untracked_players_from_url = players_from_url.get_untracked().unwrap_or(
-        FourUsers { A: Some("Player1".to_owned()), B: Some("Player2".to_owned()), C: Some("Player3".to_owned()), D: Some("Player4".to_owned()) }
+        FourUsers { a: Some("Player1".to_owned()), b: Some("Player2".to_owned()), c: Some("Player3".to_owned()), d: Some("Player4".to_owned()) }
     );
-    let a = RwSignal::new(untracked_players_from_url.A);
-    let b = RwSignal::new(untracked_players_from_url.B);
-    let c = RwSignal::new(untracked_players_from_url.C);
-    let d = RwSignal::new(untracked_players_from_url.D);
+    // yes, four signals are very necessary
+    let a = RwSignal::new(untracked_players_from_url.a.unwrap_or("???".to_owned()));
+    let b = RwSignal::new(untracked_players_from_url.b.unwrap_or("???".to_owned()));
+    let c = RwSignal::new(untracked_players_from_url.c.unwrap_or("???".to_owned()));
+    let d = RwSignal::new(untracked_players_from_url.d.unwrap_or("???".to_owned()));
     view! {
-        <p>"fr"</p>
+        
+        <p>"User A: " {a}</p>
+        <p>"User B: " {b}</p>
+        <p>"User C: " {c}</p>
+        <p>"User D: " {d}</p>
+        
     }.into_any()
 }
